ec2b5a95ca3a10b9a1601f5dbe7d13334c7811f6
GXLing <1714945118@qq.com>
2025-09-27 22:36:45 +0800

    feat: 实现页面上半部分并添加代码注释

--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 export default function Test() {
   return (
     <div className={styles.page}>
-      <div className={styles.topButtonBox}>
+      <div className={styles.header}>
         <svg className={styles.icon} viewBox='0 0 14 44' xmlns='http://www.w3.org/2000/svg'>
           <path d='m13.0729 17.6825a3.61 3.61 0 0 0 -1.7248 3.0365 3.5132 3.5132 0 0 0 2.1379 3.2223 8.394 8.394 0 0 1 -1.0948 2.2618c-.6816.9812-1.3943 1.9623-2.4787 1.9623s-1.3633-.63-2.613-.63c-1.2187 0-1.6525.6507-2.644.6507s-1.6834-.9089-2.4787-2.0243a9.7842 9.7842 0 0 1 -1.6628-5.2776c0-3.0984 2.014-4.7405 3.9969-4.7405 1.0535 0 1.9314.6919 2.5924.6919.63 0 1.6112-.7333 2.8092-.7333a3.7579 3.7579 0 0 1 3.1604 1.5802zm-3.7284-2.8918a3.5615 3.5615 0 0 0 .8469-2.22 1.5353 1.5353 0 0 0 -.031-.32 3.5686 3.5686 0 0 0 -2.3445 1.2084 3.4629 3.4629 0 0 0 -.8779 2.1585 1.419 1.419 0 0 0 .031.2892 1.19 1.19 0 0 0 .2169.0207 3.0935 3.0935 0 0 0 2.1586-1.1368z'></path>
         </svg>
@@ -27,33 +27,31 @@ export default function Test() {
           用 iPhone 13 或后续机型来换购 iPhone 17 Pro,预计可享 RMB 1100 至 RMB 6550 的折抵优惠。
         </div>
       </div>
-      <div className={styles.TopVideoWrapper}>
+      <div className={styles.topVideoWrapper}>
+        {/*<Image src='/test2.gif'  width={800} height={600} alt={'error'} />*/}
+        <Image
+          className={styles.img2}
+          src='/images/iPhone17ProMax/hero__bdntboqignj6_large_2x.jpg'
+          width={2880}
+          height={1682}
+          alt={'iPhone 17 Pro Max Image'} />
+        <div className={styles.themeDark}>
+          <div className={styles.button}>购买</div>
+          <div className={styles.text}>RMB 375/月起或 RMB 8999 起**</div>
+        </div>
+      </div>
+      <div className={styles.videobox}>
+        <div className={styles.sectionHeader}>
+          <div className={styles.headline}>先看重点</div>
+          <div className={styles.headLink}>观看影片</div>
+        </div>
         <Image src='/test2.gif' width={600} height={800} alt={'error'} />
-        <Image src='/Apple.png' width={600} height={800} alt={'error'} />
+        <Image src='/test2.gif' width={600} height={800} alt={'error'} />
+        <Image src='/test2.gif' width={600} height={800} alt={'error'} />
         <div className={styles.text2}>
           This page is used to test the GXLing's Next.js project. It is not a real page, but a test
           page.
         </div>
-      </div>
-      <div className={styles.VideoBox}>
-        <div className={styles.SectionHeader}>
-          <div className={styles.headline}>
-            先看重点
-          </div>
-          <div className={styles.headlink1}>
-            观看影片
-          </div>
-        </div>
-        <Image src='/test2.gif' width={600} height={800} alt={'error'} />
-        <Image src='/test2.gif' width={600} height={800} alt={'error'} />
-        <Image src='/test2.gif' width={600} height={800} alt={'error'} />
-        <div className={styles.text2}>
-          This page is used to test the GXLing's Next.js project. It is not a real page, but a test
-          page.
-        </div>
-      </div>
-      <div className={styles.box}>
-        <div className={styles.title}>This is GXLing's test page</div>
       </div>
     </div>
   )
